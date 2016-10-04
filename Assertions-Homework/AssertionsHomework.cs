ee7d7e0f372451de5cde6489b216ae68ee129b7b
radoslav-vl <radoslav_vl@hotmail.com>
2016-10-04 23:59:00 +0300

    Development-Tools
    This is my homework

---
+++ Assertions-Homework/AssertionsHomework.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics;
+
+using log4net;
+using log4net.Config;
+
+internal class AssertionsHomework
+{
+    private static readonly ILog Log = LogManager.GetLogger(typeof(AssertionsHomework));
+
+    public static void SelectionSort<T>(T[] arr) where T : IComparable<T>
+    {
+        for (int index = 0; index < arr.Length - 1; index++)
+        {
+            int minElementIndex = FindMinElementIndex(arr, index, arr.Length - 1);
+            Swap(ref arr[index], ref arr[minElementIndex]);
+        }
+    }
+
+    public static int BinarySearch<T>(T[] arr, T value) where T : IComparable<T>
+    {
+        return BinarySearch(arr, value, 0, arr.Length - 1);
+    }
+
+    private static int FindMinElementIndex<T>(T[] arr, int startIndex, int endIndex)
+        where T : IComparable<T>
+    {
+        Debug.Assert(arr != null, "Array is NULL");
+        Debug.Assert(arr.Length > 1, "The Array must have more than one element");
+        Debug.Assert(startIndex >= 0 && endIndex <= arr.Length - 1, "Index out of range!");
+
+        int minElementIndex = startIndex;
+        for (int i = startIndex + 1; i <= endIndex; i++)
+        {
+            if (arr[i].CompareTo(arr[minElementIndex]) < 0)
+            {
+                minElementIndex = i;
+            }
+        }
+
+        return minElementIndex;
+    }
+
+    private static void Swap<T>(ref T x, ref T y)
+    {
+        Debug.Assert(x.GetHashCode() != y.GetHashCode(), "Values are equal");
+
+        T oldX = x;
+        x = y;
+        y = oldX;
+    }
+
+    private static int BinarySearch<T>(T[] arr, T value, int startIndex, int endIndex)
+        where T : IComparable<T>
+    {
+        Debug.Assert(arr != null, "Array is NULL");
+        Debug.Assert(arr.Length > 1, "The Array must have more than one element");
+
+        while (startIndex <= endIndex)
+        {
+            int midIndex = (startIndex + endIndex) / 2;
+            if (arr[midIndex].Equals(value))
+            {
+                return midIndex;
+            }
+
+            if (arr[midIndex].CompareTo(value) < 0)
+            {
+                // Search on the right half
+                startIndex = midIndex + 1;
+            }
+            else
+            {
+                // Search on the right half
+                endIndex = midIndex - 1;
+            }
+        }
+
+        // Searched value not found
+        return -1;
+    }
+
+    private static void Main()
+    {
+        XmlConfigurator.Configure();
+        Console.WriteLine(Log.GetType());
+        Log.Info("This is test logging of info message");
+
+        int[] arr = new int[] { 3, -1, 15, 4, 17, 2, 33, 0 };
+        Console.WriteLine("arr = [{0}]", string.Join(", ", arr));
+        SelectionSort(arr);
+        Console.WriteLine("sorted = [{0}]", string.Join(", ", arr));
+
+        SelectionSort(new int[0]); // Test sorting empty array
+        SelectionSort(new int[1]); // Test sorting single element array
+
+        Console.WriteLine(BinarySearch(arr, -1000));
+        Console.WriteLine(BinarySearch(arr, 0));
+        Console.WriteLine(BinarySearch(arr, 17));
+        Console.WriteLine(BinarySearch(arr, 10));
+        Console.WriteLine(BinarySearch(arr, 1000));
+    }
+}
